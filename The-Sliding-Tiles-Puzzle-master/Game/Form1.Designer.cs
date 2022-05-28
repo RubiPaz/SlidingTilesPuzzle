2e88fd58c61cdc2dad000ed0e86da181fd0a85ac
Reuven Paz <87244634+RubiPaz@users.noreply.github.com>
2022-05-28 18:54:46 +0300

    Add files via upload

---
+++ The-Sliding-Tiles-Puzzle-master/Game/Form1.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.b3 = new MetroFramework.Controls.MetroButton();
+            this.b6 = new MetroFramework.Controls.MetroButton();
+            this.b8 = new MetroFramework.Controls.MetroButton();
+            this.b9 = new MetroFramework.Controls.MetroButton();
+            this.b4 = new MetroFramework.Controls.MetroButton();
+            this.b1 = new MetroFramework.Controls.MetroButton();
+            this.b2 = new MetroFramework.Controls.MetroButton();
+            this.b5 = new MetroFramework.Controls.MetroButton();
+            this.b7 = new MetroFramework.Controls.MetroButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // b3
+            // 
+            this.b3.BackColor = System.Drawing.Color.White;
+            this.b3.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.b3.Location = new System.Drawing.Point(442, 41);
+            this.b3.Margin = new System.Windows.Forms.Padding(4);
+            this.b3.Name = "b3";
+            this.b3.Size = new System.Drawing.Size(130, 102);
+            this.b3.TabIndex = 19;
+            this.b3.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.b3.UseCustomBackColor = true;
+            this.b3.UseSelectable = true;
+            this.b3.Click += new System.EventHandler(this.b3_Click);
+            // 
+            // b6
+            // 
+            this.b6.BackColor = System.Drawing.Color.White;
+            this.b6.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.b6.Location = new System.Drawing.Point(442, 151);
+            this.b6.Margin = new System.Windows.Forms.Padding(4);
+            this.b6.Name = "b6";
+            this.b6.Size = new System.Drawing.Size(130, 102);
+            this.b6.TabIndex = 20;
+            this.b6.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.b6.UseCustomBackColor = true;
+            this.b6.UseSelectable = true;
+            this.b6.Click += new System.EventHandler(this.b6_Click);
+            // 
+            // b8
+            // 
+            this.b8.BackColor = System.Drawing.Color.White;
+            this.b8.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.b8.Location = new System.Drawing.Point(307, 261);
+            this.b8.Margin = new System.Windows.Forms.Padding(4);
+            this.b8.Name = "b8";
+            this.b8.Size = new System.Drawing.Size(130, 102);
+            this.b8.TabIndex = 23;
+            this.b8.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.b8.UseCustomBackColor = true;
+            this.b8.UseSelectable = true;
+            this.b8.Click += new System.EventHandler(this.b8_Click);
+            // 
+            // b9
+            // 
+            this.b9.BackColor = System.Drawing.Color.White;
+            this.b9.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.b9.Location = new System.Drawing.Point(442, 261);
+            this.b9.Margin = new System.Windows.Forms.Padding(4);
+            this.b9.Name = "b9";
+            this.b9.Size = new System.Drawing.Size(130, 102);
+            this.b9.TabIndex = 22;
+            this.b9.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.b9.UseCustomBackColor = true;
+            this.b9.UseSelectable = true;
+            this.b9.Click += new System.EventHandler(this.b9_Click);
+            // 
+            // b4
+            // 
+            this.b4.BackColor = System.Drawing.Color.White;
+            this.b4.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.b4.Location = new System.Drawing.Point(169, 151);
+            this.b4.Margin = new System.Windows.Forms.Padding(4);
+            this.b4.Name = "b4";
+            this.b4.Size = new System.Drawing.Size(130, 102);
+            this.b4.TabIndex = 28;
+            this.b4.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.b4.UseCustomBackColor = true;
+            this.b4.UseSelectable = true;
+            this.b4.Click += new System.EventHandler(this.b4_Click);
+            // 
+            // b1
+            // 
+            this.b1.BackColor = System.Drawing.Color.White;
+            this.b1.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.b1.Location = new System.Drawing.Point(169, 41);
+            this.b1.Margin = new System.Windows.Forms.Padding(4);
+            this.b1.Name = "b1";
+            this.b1.Size = new System.Drawing.Size(130, 102);
+            this.b1.TabIndex = 27;
+            this.b1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.b1.UseCustomBackColor = true;
+            this.b1.UseSelectable = true;
+            this.b1.Click += new System.EventHandler(this.b1_Click);
+            // 
+            // b2
+            // 
+            this.b2.BackColor = System.Drawing.Color.White;
+            this.b2.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.b2.Location = new System.Drawing.Point(307, 41);
+            this.b2.Margin = new System.Windows.Forms.Padding(4);
+            this.b2.Name = "b2";
+            this.b2.Size = new System.Drawing.Size(130, 102);
+            this.b2.TabIndex = 26;
+            this.b2.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.b2.UseCustomBackColor = true;
+            this.b2.UseSelectable = true;
+            this.b2.Click += new System.EventHandler(this.b2_Click);
+            // 
+            // b5
+            // 
+            this.b5.BackColor = System.Drawing.Color.White;
+            this.b5.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.b5.Location = new System.Drawing.Point(307, 151);
+            this.b5.Margin = new System.Windows.Forms.Padding(4);
+            this.b5.Name = "b5";
+            this.b5.Size = new System.Drawing.Size(130, 102);
+            this.b5.TabIndex = 25;
+            this.b5.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.b5.UseCustomBackColor = true;
+            this.b5.UseSelectable = true;
+            this.b5.Click += new System.EventHandler(this.b5_Click);
+            // 
+            // b7
+            // 
+            this.b7.BackColor = System.Drawing.Color.White;
+            this.b7.FontSize = MetroFramework.MetroButtonSize.Tall;
+            this.b7.Location = new System.Drawing.Point(169, 261);
+            this.b7.Margin = new System.Windows.Forms.Padding(4);
+            this.b7.Name = "b7";
+            this.b7.Size = new System.Drawing.Size(130, 102);
+            this.b7.TabIndex = 29;
+            this.b7.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.b7.UseCustomBackColor = true;
+            this.b7.UseSelectable = true;
+            this.b7.Click += new System.EventHandler(this.b7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(302, 397);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(79, 25);
+            this.label1.TabIndex = 32;
+            this.label1.Text = "Status :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(656, 327);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 25);
+            this.label3.TabIndex = 37;
+            this.label3.Text = "Finish";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox1.Location = new System.Drawing.Point(653, 261);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(65, 62);
+            this.pictureBox1.TabIndex = 36;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(654, 217);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(73, 25);
+            this.label4.TabIndex = 39;
+            this.label4.Text = "Restart";
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.BackgroundImage")));
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pictureBox2.Location = new System.Drawing.Point(653, 151);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(63, 62);
+            this.pictureBox2.TabIndex = 38;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(164, 397);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 25);
+            this.label5.TabIndex = 40;
+            this.label5.Text = "Steps :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 447);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.b7);
+            this.Controls.Add(this.b4);
+            this.Controls.Add(this.b1);
+            this.Controls.Add(this.b2);
+            this.Controls.Add(this.b5);
+            this.Controls.Add(this.b8);
+            this.Controls.Add(this.b9);
+            this.Controls.Add(this.b6);
+            this.Controls.Add(this.b3);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Padding = new System.Windows.Forms.Padding(27, 74, 27, 25);
+            this.Resizable = false;
+            this.Text = "Shuffle";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private MetroFramework.Controls.MetroButton b3;
+        private MetroFramework.Controls.MetroButton b6;
+        private MetroFramework.Controls.MetroButton b8;
+        private MetroFramework.Controls.MetroButton b9;
+        private MetroFramework.Controls.MetroButton b4;
+        private MetroFramework.Controls.MetroButton b1;
+        private MetroFramework.Controls.MetroButton b2;
+        private MetroFramework.Controls.MetroButton b5;
+        private MetroFramework.Controls.MetroButton b7;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ The-Sliding-Tiles-Puzzle-master/Game/Form1.cs
@@ -0,0 +1,158 @@
+﻿using MetroFramework.Forms;
+using MetroFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MetroFramework.Controls;
+
+namespace Game
+{
+    public partial class Form1 : MetroForm
+    {
+        public Button CurrentBlank;
+       
+        int StepCount;
+       
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void LoadGame()
+        {
+         
+            StepCount = 0;
+            label5.Text = "Steps : " + StepCount;
+           
+            int[] num = { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Random rand = new Random();
+            int[] RandNum = num.OrderBy(x => rand.Next()).ToArray();
+            b1.Text = RandNum[0].ToString();
+            b2.Text = RandNum[1].ToString();
+            b3.Text = RandNum[2].ToString();
+            b4.Text = RandNum[3].ToString();
+            b5.Text = RandNum[4].ToString();
+            b6.Text = RandNum[5].ToString();   
+            b7.Text = RandNum[6].ToString();
+            b8.Text = RandNum[7].ToString();
+            b9.Text = "";
+            b9.Focus();
+
+            try
+            {
+                CurrentBlank.BackColor = Color.White;
+            }
+            catch (Exception e) { }
+            CurrentBlank = b9;
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadGame();
+        }
+        public void changeTile(Button valued, Button blank)
+        {
+            if(blank.Text == "")
+            {
+                blank.Text = valued.Text;
+                blank.BackColor = Color.White;
+                valued.Text = "";        
+                StepCount += 1;
+                label5.Text = "steps : " + StepCount;
+                CurrentBlank = valued;
+            }
+        }
+        private void b1_Click(object sender, EventArgs e)
+        {
+            changeTile(b1, b2);
+            changeTile(b1, b4);
+        }
+        private void b2_Click(object sender, EventArgs e)
+        {      
+            changeTile(b2, b1);
+            changeTile(b2, b3);
+            changeTile(b2, b5);
+        }
+        private void b3_Click(object sender, EventArgs e)
+        {
+            changeTile(b3, b2);
+            changeTile(b3, b6);
+        }
+        private void b4_Click(object sender, EventArgs e)
+        {       
+            changeTile(b4, b1);
+            changeTile(b4, b5);
+            changeTile(b4, b7);
+        }
+        private void b5_Click(object sender, EventArgs e)
+        {         
+            changeTile(b5, b2);
+            changeTile(b5, b4);
+            changeTile(b5, b6);
+            changeTile(b5, b8);
+        }
+        private void b6_Click(object sender, EventArgs e)
+        {         
+            changeTile(b6, b3);
+            changeTile(b6, b5);
+            changeTile(b6, b9);          
+        }
+        private void b7_Click(object sender, EventArgs e)
+        {      
+            changeTile(b7, b4);
+            changeTile(b7, b8);         
+        }
+        private void b8_Click(object sender, EventArgs e)
+        {    
+            changeTile(b8, b5);
+            changeTile(b8, b7);
+            changeTile(b8, b9);
+
+        }
+        private void b9_Click(object sender, EventArgs e)
+        {     
+            changeTile(b9, b6);
+            changeTile(b9, b8);
+        }
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            string[] resultSet = new string[8];
+            resultSet[0] = b1.Text;
+            resultSet[1] = b2.Text;
+            resultSet[2] = b3.Text;          
+            resultSet[3] = b4.Text;
+            resultSet[4] = b5.Text;
+            resultSet[5] = b6.Text;
+            resultSet[6] = b7.Text;
+            resultSet[7] = b8.Text;
+     
+            int count = 1;
+            foreach (string s in resultSet)
+            {
+                try
+                {
+                    if (Convert.ToInt32(s) == count)
+                        count += 1;
+                    else
+                        label1.Text = "Status: You Loose !";
+                }
+                catch (Exception exc) { }
+            }
+            if (count == 8)
+                label1.Text = "Status: You Win !";
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            LoadGame();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
